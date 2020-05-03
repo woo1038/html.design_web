658995dbd25e626bc5db63bddada75aa3e771a33
hyukim <woo1038@hanmail.net>
2020-05-03 17:56:24 +0900

    토글 오류 수정 및 파일 정리

--- js/main.js
+++ js/main.js
@@ -3,15 +3,18 @@
 
     const $toggles = document.querySelectorAll('.toggle');
     const $toggleBtn = document.getElementById('toggle-btn');
+    const $fixed = document.querySelectorAll('.fixed');
 
     $toggleBtn.addEventListener('click', function () {
         toggleElements();
+        fixedElements();
     });
 
     /* 브라우저 크기가 조절될때 */
     window.addEventListener('resize' ,function() {
         if(window.innerWidth > 1024) {
             offElements();
+            relativeElements();
         }
     });
 
@@ -24,6 +27,18 @@
     function offElements() {
         [].forEach.call($toggles, function(toggle) {
             toggle.classList.remove('on');          /* 클릭시 CLASS이름에 toggle위에 on추가 */
+        });
+    }   
+
+    function fixedElements() {
+        [].forEach.call($fixed, function(fixed) {
+            fixed.classList.toggle('on');          /* 클릭시 CLASS이름에 toggle위에 on추가 */
+        });
+    }
+
+    function relativeElements() {
+        [].forEach.call($toggles, function(fixed) {
+            fixed.classList.remove('on');          /* 클릭시 CLASS이름에 toggle위에 on추가 */
         });
     }   
 })(window, document)
